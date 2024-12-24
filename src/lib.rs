5f06b52336f6d1e032f5cdef9d5a2950f91d1078
Sven Toye <cool_cucumber_cat@outlook.com>
2024-12-24 23:55:37 +0100

    lib.rs doc (todo)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! formatting stuff
+//!
+//! TODO:
+//! - this documentation and stuff, readme
+//! - all documentation
+//! - double check project signature (doc alias, doc hidden, naming)
+//! - macros to declare structs for formatting not just using
+//! - clean up
+//! - testing
+//! - git and github
+//! - reduce repetetion and use good style and techniques. more macro recursion instead of repetition
+//! - reduce repetition in fmt structs (no unit struct, just field struct with no fields)
+//! - better name for fmt structs (FmtTemp etc...)
+//! - colours and styling (ascii only, no win7 (win10 is almost obsolete, so win7 definitely not worth it either, also no devs use win7))
+
 #![expect(internal_features)]
 #![feature(
     never_type,
