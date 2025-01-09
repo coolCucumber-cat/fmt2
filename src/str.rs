b3b3061b8f2707938b9a840b1eaa5c30a77fd5b7
Sven Toye <sven.toye@twofold.swiss>
2025-01-09 16:35:56 +0100

    overhauled string fmt. str and static str must be implemented separately, it is no longer automatic

--- src/str.rs
+++ src/str.rs
@@ -1,101 +1,99 @@
-use crate::{write::Write, write_to::WriteTo};
-
-use core::ops::Deref;
+use crate::write_to::FmtAdvanced;
 
 pub trait ConstStr {
     const CONST_STR: &'static str;
 }
 
-pub trait StaticStrAndStr {
-    fn static_str_and_str(&self) -> &'static str;
+pub trait FmtStaticStrImpl {
+    fn fmt_static_str_impl(&self) -> &'static str;
 }
 
-impl<T> StaticStrAndStr for T
+impl<T> FmtStaticStrImpl for T
 where
     T: ConstStr + ?Sized,
 {
     #[inline]
-    fn static_str_and_str(&self) -> &'static str {
+    fn fmt_static_str_impl(&self) -> &'static str {
         Self::CONST_STR
     }
 }
 
-trait StaticStrInternal {
-    fn static_str_internal(self) -> &'static str;
+trait FmtStaticStrInternal {
+    fn fmt_static_str_internal(self) -> &'static str;
 }
 
-impl<T> StaticStrInternal for &T
+impl<T> FmtStaticStrInternal for &T
 where
-    T: StaticStrAndStr + ?Sized,
+    T: FmtStaticStrImpl + ?Sized,
 {
     #[inline]
-    fn static_str_internal(self) -> &'static str {
-        self.static_str_and_str()
+    fn fmt_static_str_internal(self) -> &'static str {
+        self.fmt_static_str_impl()
     }
 }
 
-impl StaticStrInternal for &'static str {
-    fn static_str_internal(self) -> &'static str {
+impl FmtStaticStrInternal for &'static str {
+    fn fmt_static_str_internal(self) -> &'static str {
         self
     }
 }
 
-pub trait StaticStr<'a> {
-    fn static_str(&'a self) -> &'static str;
+pub trait FmtStaticStr<'a> {
+    fn fmt_static_str(&'a self) -> &'static str;
 }
 
-impl<'a, T> StaticStr<'a> for T
+impl<'a, T> FmtStaticStr<'a> for T
 where
     T: 'a,
-    &'a T: StaticStrInternal,
+    &'a T: FmtStaticStrInternal,
 {
     #[inline]
-    fn static_str(&'a self) -> &'static str {
-        StaticStrInternal::static_str_internal(self)
+    fn fmt_static_str(&'a self) -> &'static str {
+        FmtStaticStrInternal::fmt_static_str_internal(self)
     }
 }
 
-pub trait Str {
-    fn str(&self) -> &str;
-}
+// pub trait Str {
+//     fn str(&self) -> &str;
+// }
+//
+// impl<T> Str for T
+// where
+//     T: ImplStaticStr + ?Sized,
+// {
+//     #[inline]
+//     fn str(&self) -> &str {
+//         self.impl_static_str()
+//     }
+// }
+//
+// impl<T> WriteTo for T
+// where
+//     T: Str + ?Sized,
+// {
+//     #[inline]
+//     fn write_to<W>(&self, w: &mut W) -> Result<(), W::Error>
+//     where
+//         W: Write + ?Sized,
+//     {
+//         w.write_str(self.str())
+//     }
+//
+//     #[inline]
+//     fn len_hint(&self) -> usize {
+//         self.str().len()
+//     }
+// }
+//
+// impl Str for str {
+//     #[inline]
+//     fn str(&self) -> &str {
+//         self
+//     }
+// }
 
-impl<T> Str for T
-where
-    T: StaticStrAndStr + ?Sized,
-{
-    #[inline]
-    fn str(&self) -> &str {
-        self.static_str_and_str()
-    }
-}
-
-impl<T> WriteTo for T
-where
-    T: Str + ?Sized,
-{
-    #[inline]
-    fn write_to<W>(&self, w: &mut W) -> Result<(), W::Error>
-    where
-        W: Write + ?Sized,
-    {
-        w.write_str(self.str())
-    }
-
-    #[inline]
-    fn len_hint(&self) -> usize {
-        self.str().len()
-    }
-}
-
-impl Str for str {
-    #[inline]
-    fn str(&self) -> &str {
-        self
-    }
-}
-
-impl StaticStrAndStr for bool {
-    fn static_str_and_str(&self) -> &'static str {
+impl FmtStaticStrImpl for bool {
+    fn fmt_static_str_impl(&self) -> &'static str {
         if *self {
             "true"
         } else {
@@ -104,59 +102,66 @@ impl StaticStrAndStr for bool {
     }
 }
 
-#[derive(Clone, Copy)]
-pub struct WithStr<T, U = &'static str> {
-    pub value: T,
-    pub str: U,
-}
-
-impl<T, U> WithStr<T, U> {
-    pub const fn new(value: T, str: U) -> Self {
-        Self { value, str }
-    }
-
-    pub fn map_value<V>(self, f: impl FnOnce(T) -> V) -> WithStr<V, U> {
-        WithStr {
-            value: f(self.value),
-            str: self.str,
-        }
-    }
-
-    pub fn replace_value<V>(self, value: V) -> WithStr<V, U> {
-        WithStr {
-            value,
-            str: self.str,
-        }
+impl FmtAdvanced for bool {
+    type Target = str;
+    fn fmt_advanced(&self) -> &Self::Target {
+        self.fmt_static_str_impl()
     }
 }
 
-impl<T, U> Str for WithStr<T, &'_ U>
-where
-    U: Str + ?Sized,
-{
-    fn str(&self) -> &str {
-        self.str.str()
-    }
-}
-
-impl<T0, T1, U> AsRef<T1> for WithStr<T0, U>
-where
-    T0: AsRef<T1>,
-    T1: ?Sized,
-{
-    #[inline]
-    fn as_ref(&self) -> &T1 {
-        self.value.as_ref()
-    }
-}
-
-impl<T, U> Deref for WithStr<T, U> {
-    type Target = Self;
-
-    fn deref(&self) -> &Self::Target {
-        self
-    }
-}
+// #[derive(Clone, Copy)]
+// pub struct WithStr<T, U = &'static str> {
+//     pub value: T,
+//     pub str: U,
+// }
+//
+// impl<T, U> WithStr<T, U> {
+//     pub const fn new(value: T, str: U) -> Self {
+//         Self { value, str }
+//     }
+//
+//     pub fn map_value<V>(self, f: impl FnOnce(T) -> V) -> WithStr<V, U> {
+//         WithStr {
+//             value: f(self.value),
+//             str: self.str,
+//         }
+//     }
+//
+//     pub fn replace_value<V>(self, value: V) -> WithStr<V, U> {
+//         WithStr {
+//             value,
+//             str: self.str,
+//         }
+//     }
+// }
+//
+// impl<T, U> Str for WithStr<T, &'_ U>
+// where
+//     U: Str + ?Sized,
+// {
+//     fn str(&self) -> &str {
+//         self.str.str()
+//     }
+// }
+//
+// impl<T0, T1, U> AsRef<T1> for WithStr<T0, U>
+// where
+//     T0: AsRef<T1>,
+//     T1: ?Sized,
+// {
+//     #[inline]
+//     fn as_ref(&self) -> &T1 {
+//         self.value.as_ref()
+//     }
+// }
+//
+// impl<T, U> Deref for WithStr<T, U> {
+//     type Target = Self;
+//
+//     fn deref(&self) -> &Self::Target {
+//         self
+//     }
+// }
 
 #[macro_export]
 macro_rules! impl_const_str_for {
