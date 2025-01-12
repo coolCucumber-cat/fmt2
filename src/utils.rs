d530f5704c98fc9bf585940eece9e56ab073d724
Antifa Communist <>
2025-01-12 17:04:23 +0100

    testing 123

--- src/utils.rs
+++ src/utils.rs
@@ -39,75 +39,75 @@ unsafe impl<T> SafeTransmuteFrom<T> for T {
 //         unsafe { ::core::mem::transmute(value) }
 //     }
 // }
-
-#[macro_export]
-macro_rules! sussy {
-    () => {};
-}
-
-#[macro_export]
-macro_rules! enum_alias {
-    {
-        $(#[$meta:meta])*
-        enum $name:ident: $ty:ty = $({
-            $variant0:ident $(| $variant:ident)*
-        })?;
-    } => {
-        #[repr(u8)]
-        #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-        $(#[$meta])*
-        enum $name {
-            $(
-                $variant0 = <$ty>::$variant0 as u8,
-                $($variant = <$ty>::$variant as u8, )*
-            )?
-        }
-
-        unsafe impl ImplSafeTransmuteFrom<$name> for $ty {
-            #[inline]
-            fn impl_safe_transmute_from(value: $name) -> Self {
-                unsafe { ::core::mem::transmute(value) }
-            }
-        }
-
-        impl ::core::convert::From<$name> for $ty {
-            #[inline]
-            fn from(value: $name) -> Self {
-                #[cfg(debug_assertions)]
-                let self_dev: Self = match value {
-                    $(
-                        <$name>::$variant0 => <$ty>::$variant0,
-                        $(
-                            <$name>::$variant => <$ty>::$variant,
-                        )*
-                    )?
-                };
-                let self_prod: Self = unsafe { ::core::mem::transmute(value) };
-                #[cfg(debug_assertions)]
-                {
-                    ::core::debug_assert_eq!(self_dev, self_prod, ::core::concat!(::core::stringify!(::core::convert::From<$name> for $ty)));
-                }
-                self_prod
-            }
-        }
-
-        impl TryFrom<$ty> for $name {
-            type Error = ();
-            #[inline]
-            fn try_from(value: $ty) -> Result<Self, Self::Error> {
-                match value {
-                    $(
-                        <$ty>::$variant0 => ::core::result::Result::Ok(<$name>::$variant0),
-                        $(
-                            <$ty>::$variant => ::core::result::Result::Ok(<$name>::$variant),
-                        )*
-                        _ => ::core::result::Result::Err(()),
-                    )?
-                }
-            }
-        }
-    };
-}
+//
+// #[macro_export]
+// macro_rules! sussy {
+//     () => {};
+// }
+//
+// #[macro_export]
+// macro_rules! enum_alias {
+//     {
+//         $(#[$meta:meta])*
+//         enum $name:ident: $ty:ty = $({
+//             $variant0:ident $(| $variant:ident)*
+//         })?;
+//     } => {
+//         #[repr(u8)]
+//         #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+//         $(#[$meta])*
+//         enum $name {
+//             $(
+//                 $variant0 = <$ty>::$variant0 as u8,
+//                 $($variant = <$ty>::$variant as u8, )*
+//             )?
+//         }
+//
+//         unsafe impl $crate::utils::ImplSafeTransmuteFrom<$name> for $ty {
+//             #[inline]
+//             fn impl_safe_transmute_from(value: $name) -> Self {
+//                 unsafe { ::core::mem::transmute(value) }
+//             }
+//         }
+//
+//         impl ::core::convert::From<$name> for $ty {
+//             #[inline]
+//             fn from(value: $name) -> Self {
+//                 #[cfg(debug_assertions)]
+//                 let self_dev: Self = match value {
+//                     $(
+//                         <$name>::$variant0 => <$ty>::$variant0,
+//                         $(
+//                             <$name>::$variant => <$ty>::$variant,
+//                         )*
+//                     )?
+//                 };
+//                 let self_prod: Self = unsafe { ::core::mem::transmute(value) };
+//                 #[cfg(debug_assertions)]
+//                 {
+//                     ::core::debug_assert_eq!(self_dev, self_prod, ::core::concat!(::core::stringify!(::core::convert::From<$name> for $ty)));
+//                 }
+//                 self_prod
+//             }
+//         }
+//
+//         impl ::core::convert::TryFrom<$ty> for $name {
+//             type Error = ();
+//             #[inline]
+//             fn try_from(value: $ty) -> Result<Self, Self::Error> {
+//                 match value {
+//                     $(
+//                         <$ty>::$variant0 => ::core::result::Result::Ok(<$name>::$variant0),
+//                         $(
+//                             <$ty>::$variant => ::core::result::Result::Ok(<$name>::$variant),
+//                         )*
+//                         _ => ::core::result::Result::Err(()),
+//                     )?
+//                 }
+//             }
+//         }
+//     };
+// }
 
 // #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 // enum X {
