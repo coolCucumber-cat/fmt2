f3ae104b2e443fc83f7337362739c05f7fdc4662
Antifa Communist <>
2025-01-12 16:57:01 +0100

    utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,44 +1,122 @@
-#[inline]
-#[must_use]
-pub fn has_newlines(s: &str) -> bool {
-    let v = s.contains('\n');
-    #[cfg(debug_assertions)]
-    {
-        let v2 = s.chars().any(|c| c == '\n');
-        debug_assert_eq!(v, v2);
-        let count = count_newlines(s);
-        debug_assert_eq!(v, count != 0);
+/// # Safety
+/// Only implement this trait if transmuting from `T` to `Self` and vice versa is safe
+pub unsafe trait ImplSafeTransmuteFrom<T>: Sized {
+    fn impl_safe_transmute_from(value: T) -> Self;
+}
+
+// unsafe impl<T> ImplSafeTransmuteFrom<T> for T {
+//     fn impl_safe_transmute_from(value: T) -> Self {
+//         value
+//     }
+// }
+
+unsafe impl<T, U> ImplSafeTransmuteFrom<&[T]> for &[U]
+where
+    U: ImplSafeTransmuteFrom<T>,
+{
+    fn impl_safe_transmute_from(value: &[T]) -> Self {
+        unsafe { ::core::mem::transmute(value) }
     }
-    v
 }
 
-#[inline]
-#[must_use]
-pub fn count_newlines(s: &str) -> usize {
-    s.chars().filter(|&c| c == '\n').count()
+/// # Safety
+/// Only implement this trait if transmuting from `T` to `Self` and vice versa is safe
+pub unsafe trait SafeTransmuteFrom<T>: Sized {
+    fn safe_transmute_from(value: T) -> Self;
 }
 
-#[inline]
-#[must_use]
-pub fn first_line(s: &str) -> &str {
-    let s = first_line_no_debug_assertion(s);
-    #[cfg(debug_assertions)]
-    {
-        debug_assert!(!has_newlines(s));
-        debug_assert_eq!(count_newlines(s), 0);
+unsafe impl<T> SafeTransmuteFrom<T> for T {
+    fn safe_transmute_from(value: T) -> Self {
+        value
     }
-    s
 }
 
-#[inline]
-#[must_use]
-pub fn first_line_no_debug_assertion(s: &str) -> &str {
-    let s = match s.find('\n') {
-        Some(i) => unsafe { s.get_unchecked(..i) },
-        None => s,
+// unsafe impl<T, U> SafeTransmuteFrom<&[T]> for &[U]
+// where
+//     U: SafeTransmuteFrom<T>,
+// {
+//     fn safe_transmute_from(value: &[T]) -> Self {
+//         unsafe { ::core::mem::transmute(value) }
+//     }
+// }
+
+#[macro_export]
+macro_rules! sussy {
+    () => {};
+}
+
+#[macro_export]
+macro_rules! enum_alias {
+    {
+        $(#[$meta:meta])*
+        enum $name:ident: $ty:ty = $({
+            $variant0:ident $(| $variant:ident)*
+        })?;
+    } => {
+        #[repr(u8)]
+        #[derive(Debug, Copy, Clone, PartialEq, Eq)]
+        $(#[$meta])*
+        enum $name {
+            $(
+                $variant0 = <$ty>::$variant0 as u8,
+                $($variant = <$ty>::$variant as u8, )*
+            )?
+        }
+
+        unsafe impl ImplSafeTransmuteFrom<$name> for $ty {
+            #[inline]
+            fn impl_safe_transmute_from(value: $name) -> Self {
+                unsafe { ::core::mem::transmute(value) }
+            }
+        }
+
+        impl ::core::convert::From<$name> for $ty {
+            #[inline]
+            fn from(value: $name) -> Self {
+                #[cfg(debug_assertions)]
+                let self_dev: Self = match value {
+                    $(
+                        <$name>::$variant0 => <$ty>::$variant0,
+                        $(
+                            <$name>::$variant => <$ty>::$variant,
+                        )*
+                    )?
+                };
+                let self_prod: Self = unsafe { ::core::mem::transmute(value) };
+                #[cfg(debug_assertions)]
+                {
+                    ::core::debug_assert_eq!(self_dev, self_prod, ::core::concat!(::core::stringify!(::core::convert::From<$name> for $ty)));
+                }
+                self_prod
+            }
+        }
+
+        impl TryFrom<$ty> for $name {
+            type Error = ();
+            #[inline]
+            fn try_from(value: $ty) -> Result<Self, Self::Error> {
+                match value {
+                    $(
+                        <$ty>::$variant0 => ::core::result::Result::Ok(<$name>::$variant0),
+                        $(
+                            <$ty>::$variant => ::core::result::Result::Ok(<$name>::$variant),
+                        )*
+                        _ => ::core::result::Result::Err(()),
+                    )?
+                }
+            }
+        }
     };
-    s
 }
+
+// #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+// enum X {
+//     A,
+//     B,
+//     C,
+// }
+//
+// enum_alias! { #[doc(alias = "sussy")] #[doc = "alias"] enum Y: X = { C | B }; }
 
 #[allow(unused)]
 #[cfg(test)]
@@ -46,14 +124,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_first_line() {
-        assert_eq!(first_line_no_debug_assertion("123"), "123");
-        assert_eq!(first_line_no_debug_assertion("123\nabc"), "123");
-        assert_eq!(first_line_no_debug_assertion("123\n"), "123");
-        assert_eq!(first_line_no_debug_assertion("123\nabc\n456"), "123");
-        assert_eq!(first_line_no_debug_assertion("123\nabc\n"), "123");
-        assert_eq!(first_line_no_debug_assertion("\nabc"), "");
-        assert_eq!(first_line_no_debug_assertion("\n"), "");
-        assert_eq!(first_line_no_debug_assertion(""), "");
-    }
+    fn test() {}
 }
