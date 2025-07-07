802ba7fb1fd28ba532abdba4fac88e42d5bddae5
Antifa Communist <>
2025-07-07 11:01:11 +0200

    Refactor macros

--- fmt2_macros/src/fmt/stage1.rs
+++ fmt2_macros/src/fmt/stage1.rs
@@ -34,9 +34,12 @@ impl Parse for Writer {
     }
 }
 
-enum Fallibility {
+#[derive(Default)]
+pub enum Fallibility {
     Fallible,
     Infallible,
+    #[default]
+    Ignore,
 }
 impl Parse for Fallibility {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
@@ -44,30 +47,34 @@ impl Parse for Fallibility {
             .parse::<Token![?]>()
             .map(|_| Fallibility::Fallible)
             .or_else(|_| input.parse::<Token![!]>().map(|_| Fallibility::Infallible))
+            .or(Ok(Fallibility::Ignore))
     }
 }
 
 #[derive(Default)]
-struct WriterSettings {
-    fallibility: Option<Fallibility>,
+pub struct WriterArgs {
+    fallibility: Fallibility,
 }
-impl Parse for WriterSettings {
+impl Parse for WriterArgs {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         input.parse::<Token![;]>()?;
-        let fallibility = input.parse::<Fallibility>().ok();
-        Ok(WriterSettings { fallibility })
+        let fallibility = input.parse::<Fallibility>()?;
+        Ok(WriterArgs { fallibility })
     }
 }
 
 pub struct FmtModeWriter {
     writer: Writer,
-    settings: WriterSettings,
+    writer_args: WriterArgs,
 }
 impl Parse for FmtModeWriter {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let writer = input.parse::<Writer>()?;
-        let settings = input.parse::<WriterSettings>().unwrap_or_default();
-        Ok(Self { writer, settings })
+        let writer_args = input.parse::<WriterArgs>().unwrap_or_default();
+        Ok(Self {
+            writer,
+            writer_args,
+        })
     }
 }
 
@@ -78,65 +85,77 @@ pub enum FmtMode {
 impl Parse for FmtMode {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let content = parse_round_brackets(input)?;
-        let writer = content.parse::<FmtModeWriter>();
-        match writer {
-            Ok(writer) => Ok(FmtMode::Writer(writer)),
-            Err(_) => Ok(FmtMode::String),
-        }
+        content
+            .parse::<FmtModeWriter>()
+            .map(FmtMode::Writer)
+            .or(Ok(FmtMode::String))
     }
 }
 
-struct FmtTokenLit(Expr);
-
+pub struct FmtTokenLit(Expr);
 impl Parse for FmtTokenLit {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
-        if let Ok(lit) = input.parse::<ExprLit>() {
-            let expr = Expr::Lit(lit);
-            Ok(Self(expr))
-        } else {
-            let content = parse_round_brackets(input)?;
-            let expr = content.parse::<Expr>()?;
-            Ok(Self(expr))
-        }
+        input
+            .parse::<ExprLit>()
+            .map(|lit| Self(Expr::Lit(lit)))
+            .or_else(|_| {
+                let content = parse_round_brackets(input)?;
+                let expr = content.parse::<Expr>()?;
+                Ok(Self(expr))
+            })
     }
 }
 
-enum FmtTokenExprArgs {
+#[derive(Default)]
+pub enum ExprMode {
+    #[default]
     None,
 }
-impl Parse for FmtTokenExprArgs {
+impl Parse for ExprMode {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         Ok(Self::None)
     }
 }
 
-struct FmtTokenExpr {
-    expr: Expr,
+#[derive(Default)]
+pub struct FmtTokenExprArgs {
+    expr_mode: ExprMode,
+}
+impl Parse for FmtTokenExprArgs {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        input.parse::<Token![;]>()?;
+        let expr_mode = input.parse::<ExprMode>()?;
+        Ok(Self { expr_mode })
+    }
+}
+
+pub struct FmtTokenExpr {
+    expr: Box<Expr>,
     args: FmtTokenExprArgs,
 }
 impl Parse for FmtTokenExpr {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let content = parse_curly_brackets(input)?;
         let expr = content.parse::<Expr>()?;
-        let args = if content.parse::<Token![;]>().is_ok() {
-            content.parse::<FmtTokenExprArgs>()?
-        } else {
-            FmtTokenExprArgs::None
-        };
-        Ok(Self { expr, args })
+        let args = content.parse::<FmtTokenExprArgs>().unwrap_or_default();
+        Ok(Self {
+            expr: Box::new(expr),
+            args,
+        })
     }
 }
 
-enum FmtToken {
+pub enum FmtToken {
     Lit(Vec<FmtTokenLit>),
     Expr(FmtTokenExpr),
 }
 
-struct FmtTokens {
+pub struct FmtTokens {
     tokens: Vec<FmtToken>,
 }
 impl Parse for FmtTokens {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        // let tokens = input;
         let tokens = vec![];
 
         Ok(Self { tokens })
@@ -147,7 +166,6 @@ pub struct Fmt {
     mode: FmtMode,
     tokens: FmtTokens,
 }
-
 impl Parse for Fmt {
     fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
         let mode = input.parse::<FmtMode>()?;
